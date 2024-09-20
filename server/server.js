85ef3a597ae8b5678cc9addc9ab0f02dc0b4c45b
Bojan Bogdanovic <bojan.bogdanovic@adera.no>
2024-09-20 23:00:56 +0200

    add vercel config

--- server/server.js
+++ server/server.js
@@ -109,15 +109,15 @@ const isProduction = process.env.NODE_ENV === "production";
 console.log(isProduction)
 
 if (!isProduction) {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  // // Set static folder
+  // app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
+  // app.get("*", (req, res) => {
 
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    ); // index is in /server/src so 2 folders up
-  });
+  //   res.sendFile(
+  //     path.resolve(__dirname, "..", "client", "build", "index.html")
+  //   ); // index is in /server/src so 2 folders up
+  // });
   app.listen(process.env.PORT || 4000);
 
 } else {
