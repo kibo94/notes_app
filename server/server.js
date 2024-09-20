290752013c7bd54472f87faa556b579d527f9f7a
Bojan Bogdanovic <bojan.bogdanovic@adera.no>
2024-09-20 22:44:23 +0200

    update cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const http = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["https://notes-app-75h8.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
