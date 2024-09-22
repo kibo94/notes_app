94fcde6b8685ae0e424d1ac89ceb9c232724d002
Bojan Bogdanovic <bojan.bogdanovic@adera.no>
2024-09-22 23:28:49 +0200

    fix url

--- client/src/utilys/axios.js
+++ client/src/utilys/axios.js
@@ -1,7 +1,7 @@
 
 import axios from "axios"
 const instance = axios.create({
-  baseURL: 'https://notes-app-api-amber.vercel.app'
+  baseURL: 'https://notes-app-bjk8.vercel.app/api'
   // baseURL: "http://localhost:4000",
 
 });
